63d86b16fbcc7f73ba9232bf9264a60e9b53b4dc
Sven Matzke <matzke.sven@googlemail.com>
2019-06-23 17:34:17 +0200

    fixing async server

--- userv.async_server/userv/async_server.py
+++ userv.async_server/userv/async_server.py
@@ -1,13 +1,53 @@
-import os
 import uasyncio as asyncio
 from userv import response_header, get_mime_type, parse_request
-from userv.routing import text
+from userv.routing import text_response, json_response
 
-try:
-    import ujson
-except ImportError:
-    import json
-
+#TODO
+#Also writing tests
+# def static_file(file_name):
+#     def todo(request):
+#         return ....  # TODO
+# 
+#     return todo
+# if route.startswith('/static'):
+#             fname = route.split("/")[-1]
+#             if fname not in os.listdir():
+#                 text_response = await text_response(
+#                     "File %s is not available " % fname,
+#                     status=404
+#                 )
+#                 await writer.awrite(
+#                     text_response
+#                 )
+#             else:
+#                 mime_type = get_mime_type(fname)
+#                 if mime_type is None:
+#                     text_response = await text_response(
+#                         "",
+#                         status=415,
+#                     )
+#                     await writer.awrite(
+#                         text_response
+#                     )
+#                 else:
+#                     # serve static file
+#                     await writer.awrite(
+#                         response_header(
+#                             status=200,
+#                             content_type=mime_type,
+#                             content_length=os.stat(fname)[0]
+#                         )
+#                     )
+#                     file_ptr = open(fname)
+#                     buf = bytearray(self._buffersize) # TODO
+#                     while True:
+#                         l = file_ptr.readinto(buf)
+#                         if not l:
+#                             break
+#                         await writer.awrite(buf, 0, l)
+#                     file_ptr.close()
+# 
+#         else:
 
 def _is_async_func(func):
     """
@@ -25,58 +65,29 @@ def run_server(router, address="0.0.0.0", port=80):
     :type port: int
     """
 
-    def run_handle(self, reader, writer):
+    async def run_handle(reader, writer): #TODO das ist doch nur ein request...
         print("started")
-        complete_request = yield from reader.read()
+        complete_request = await reader.read()
         parsed_request = parse_request(complete_request.decode())
         route = parsed_request.get('route')
-        if route.startswith('/static'):
-            fname = route.split("/")[-1]
-            if fname not in os.listdir():
-                text_response = yield from text(
-                    "File %s is not available " % fname,
-                    status=404
-                )
-                yield from writer.awrite(
-                    text_response
-                )
-            else:
-                mime_type = get_mime_type(fname)
-                if mime_type is None:
-                    text_response = yield from text(
-                        "",
-                        status=415,
-                    )
-                    yield from writer.awrite(
-                        text_response
-                    )
-                else:
-                    # serve static file
-                    yield from writer.awrite(
-                        response_header(
-                            status=200,
-                            content_type=mime_type,
-                            content_length=os.stat(fname)[0]
-                        )
-                    )
-                    file_ptr = open(fname)
-                    buf = bytearray(self._buffersize) # TODO
-                    while True:
-                        l = file_ptr.readinto(buf)
-                        if not l:
-                            break
-                        yield from writer.awrite(buf, 0, l)
-                    file_ptr.close()
-
+   
+        callback = await router.get(route=route, method=parsed_request.get('method'))
+        if callback in [404, 405]:
+            response_generator = text_response("", status=callback)
+        elif _is_async_func(callback):
+            try:
+                response_generator = await callback(parsed_request)
+            except Exception as e:
+                response_generator = text_response(str(e), status=500)
         else:
-            callback = yield from router.get(route=route, method=parsed_request.get('method'))
-            if callback in [404, 405]:
-                yield from text("Requested Route or method is not available", status=callback)
-            response = yield from callback(parsed_request)
-            yield from writer.awrite(
-                response
-            )
-        yield from writer.aclose()
+            try:
+                response_generator = callback(parsed_request)
+            except Exception as e:
+                response_generator = text_response(str(e), status=500)
+
+        for line in response_generator:
+            await writer.awrite(line)
+        await writer.aclose()
 
     loop = asyncio.get_event_loop()
     loop.call_soon(
